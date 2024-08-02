680d4e51315c275f61365a762f217c05a1dae2ea
Jeon Hyeon Min <textneckdeveloper@gmail.com>
2024-08-02 10:23:01 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.04 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int[][] lines) {
+        
+        int answer = 0;
+        
+        for (int i = -100; i < 100; i++) {
+            
+            int line = 0;
+            
+            if (lines[0][0]<=i && lines[0][1]>i) line++;
+            if (lines[1][0]<=i && lines[1][1]>i) line++;
+            if (lines[2][0]<=i && lines[2][1]>i) line++;
+
+            if(line > 1) answer++;
+            
+        }
+
+        return answer;
+        
+    }
+}
