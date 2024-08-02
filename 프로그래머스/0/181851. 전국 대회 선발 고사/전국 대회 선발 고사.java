5c732899859066d6e3f3eb35847d4163da376583
Jeon Hyeon Min <textneckdeveloper@gmail.com>
2024-08-02 10:12:29 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 7.23 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -0,0 +1,14 @@
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int[] rank, boolean[] attendance) {
+        return IntStream.range(0, rank.length)
+                .filter(i -> attendance[i])
+                .boxed()
+                .sorted(Comparator.comparing(i -> rank[i]))
+                .limit(3L)
+                .reduce((current, next) -> current * 100 + next)
+                .get();
+    }
+}
