5b5454adf3a9d0dfb461e14c2b6cea07ceb1b482
Jeon Hyeon Min <textneckdeveloper@gmail.com>
2024-08-02 09:46:54 +0900

    [level 0] Title: 중복된 문자 제거, Time: 2.11 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String my_string) {
+        return Arrays.stream(my_string.split("")).distinct().collect(Collectors.joining());
+    }
+}
