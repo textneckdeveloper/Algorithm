f7baae7f465967ab13c1ee0dc9d0411fc7d55a37
Jeon Hyeon Min <textneckdeveloper@gmail.com>
2024-08-02 10:35:13 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 3.89 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        
+        int count = 1;
+        
+        while(n%count != 1){
+            count++;
+        }
+        
+        return count;
+        
+    }
+}
