042c54e922d9e01de058f0567ad42cb0dea20ff6
Jeon Hyeon Min <textneckdeveloper@gmail.com>
2024-08-02 09:36:14 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.03 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        
+        int even = 0;
+        int odd = 0;
+        
+        for(int i : num_list){
+            if(i % 2 ==0){
+                even++;
+            }
+            else{
+                odd++;
+            }
+        }
+        
+        return new int[]{even, odd};
+        
+    }
+}
