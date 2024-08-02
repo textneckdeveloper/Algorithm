6c5f794e15e23a1ff5b3d4ca007bb9d49d7b0cab
Jeon Hyeon Min <textneckdeveloper@gmail.com>
2024-08-02 09:22:54 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 29.02 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        
+        String answer = "";
+        
+        for(int i=my_string.length()-n; i<=my_string.length()-1; i++){
+            answer += my_string.charAt(i);
+        }
+        
+        return answer;
+        
+    }
+}
