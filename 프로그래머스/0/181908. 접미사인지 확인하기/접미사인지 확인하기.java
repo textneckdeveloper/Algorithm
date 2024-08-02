db4a8f893909859072bf840c33be7f8422546632
Jeon Hyeon Min <textneckdeveloper@gmail.com>
2024-08-02 09:22:37 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.06 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        
+        for(int i=0; i<my_string.length(); i++){
+            if(is_suffix.equals(my_string.substring(i, my_string.length()))){
+                return 1;
+            };
+        }
+        
+        return 0;
+        
+    }
+}
