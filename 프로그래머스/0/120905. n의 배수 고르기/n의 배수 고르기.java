418fd5639936e7059da3c685377c9e8ac2ef354f
Jeon Hyeon Min <textneckdeveloper@gmail.com>
2024-08-02 09:13:56 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.04 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i : numlist){
+            if(i % n == 0){
+                list.add(i);
+            }
+        }
+        
+        int[] answer = new int[list.size()];
+        
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+        
+    }
+}
