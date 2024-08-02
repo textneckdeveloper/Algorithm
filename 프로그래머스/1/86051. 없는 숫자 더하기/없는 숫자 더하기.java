2bd1af9c7b645083220c104b616523eae198882b
Jeon Hyeon Min <textneckdeveloper@gmail.com>
2024-08-02 14:10:36 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] numbers) {
+        
+        int sum = 45;
+        
+        for (int i : numbers) {
+            sum -= i;
+        }
+        
+        return sum;
+        
+    }
+}
