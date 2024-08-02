11a96ee84f08ae0d0d6726fb2c7e9de6fe5b87cb
Jeon Hyeon Min <textneckdeveloper@gmail.com>
2024-08-02 09:35:02 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n, int t) {
+        
+        for(int i=0; i<t; i++){
+            n = n*2;
+        }
+        
+        return n;
+        
+    }
+}
