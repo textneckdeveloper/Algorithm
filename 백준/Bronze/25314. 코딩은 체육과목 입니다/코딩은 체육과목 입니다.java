523922305ff93357f5330dc256b20ad67b1b8e92
Jeon Hyeon Min <textneckdeveloper@gmail.com>
2024-08-12 16:08:45 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 172 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=1; i<=(N/4); i++){
+            System.out.print("long ");
+        }
+        System.out.print("int");
+        sc.close();
+    }
+}
