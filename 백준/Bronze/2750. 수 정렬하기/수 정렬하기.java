c29bd368fc9e5782af1f59a72ee06b2d0009d757
Jeon Hyeon Min <textneckdeveloper@gmail.com>
2024-09-11 18:17:58 +0900

    [Bronze II] Title: 수 정렬하기, Time: 112 ms, Memory: 14576 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -2,59 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    
-    static class Sorting {
-        int[] A;
-        Sorting(int[] B) {
-            A = B;
-        }
-        
-        void quickSort() {
-            qSort(0, A.length-1);
-        }
-        private void qSort(int p, int r) {
-            if (p < r) {
-                int q = partition(p, r);
-                qSort(0, q-1);
-                qSort(q+1, r);
-            }
-        }
-        private int partition(int p, int r) {
-            int i = p-1;
-            int pivot = A[r];
-            int tmp;
-            for (int j=p; j<r; j++) {
-                if (A[j] < pivot) {
-                    i++;
-                    tmp = A[i];
-                    A[i] = A[j];
-                    A[j] = tmp;
-                }
-            }
-            tmp = A[i+1];
-            A[i+1] = A[r];
-            A[r] = tmp;
-            return i+1;
-        }
-    }
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter pw = new PrintWriter(System.out);
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        for (int i=0; i<arr.length; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        Sorting s = new Sorting(arr);
-        s.quickSort();
-        
-        for (int i=0; i<arr.length; i++) {
-            pw.println(arr[i]);
-        }
-        
-        pw.flush();
-        br.close();
-        pw.close();
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	
+    	int n = Integer.parseInt(br.readLine());
+    	int[] arr = new int[n];
+    	
+    	for(int i=0; i<n; i++) {
+    		arr[i] = Integer.parseInt(br.readLine());
+    	}
+    	
+    	Arrays.sort(arr);
+    	
+    	for (int i=0; i<arr.length; i++) {
+    		bw.write(String.valueOf(arr[i]));
+    		bw.newLine();
+    	}
+    	
+    	bw.flush();
+    	br.close();
+    	bw.close();
     }
 }
