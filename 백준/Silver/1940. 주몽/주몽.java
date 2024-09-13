793550d8a911a665a489beb9eda1af70a9ebb240
Jeon Hyeon Min <textneckdeveloper@gmail.com>
2024-09-13 18:33:48 +0900

    [Silver IV] Title: 주몽, Time: 160 ms, Memory: 16408 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -3,42 +3,6 @@ import java.util.*;
 
 public class Main {
     
-    static class Sorting {
-		int[] A;
-		
-		Sorting(int[] B) {
-			A = B;
-		}
-		
-        void quickSort() {
-            qSort(0, A.length-1);
-		}
-        private void qSort(int p, int r) {
-            if (p < r) {
-                int q = partition(p, r);
-                qSort(p, q-1);
-                qSort(q+1, r);
-            }
-        }
-        private int partition(int p, int r) {
-            int pivot = A[r];
-            int i = p-1;
-            int tmp;
-            for (int j=p; j<r; j++) {
-                if (A[j] <= pivot) {
-                    i++;
-                    tmp = A[i];
-                    A[i] = A[j];
-                    A[j] = tmp;
-                }
-            }
-            tmp = A[i+1];
-            A[i+1] = A[r];
-            A[r] = tmp;
-            return i+1;
-        }
-	}
-    
     public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	int n = Integer.parseInt(br.readLine());
@@ -50,8 +14,7 @@ public class Main {
     		val[i] = Integer.parseInt(st.nextToken());
     	}
     	
-    	Sorting s = new Sorting(val);
-    	s.quickSort();
+        Arrays.sort(val);
     	
     	int count = 0;
     	int i = 0;
