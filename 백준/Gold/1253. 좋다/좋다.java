6a83cebea7aedc4b9c86f6456fe2b0c267d4dcea
Jeon Hyeon Min <textneckdeveloper@gmail.com>
2024-09-13 20:17:49 +0900

    [Gold IV] Title: 좋다, Time: 156 ms, Memory: 14704 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -2,43 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    
-    static class Sorting {
-		int[] A;
-		
-		Sorting(int[] B) {
-			A = B;
-		}
-		
-        void quickSort() {
-            qSort(0, A.length-1);
-		}
-        private void qSort(int p, int r) {
-            if (p < r) {
-                int q = partition(p, r);
-                qSort(p, q-1);
-                qSort(q+1, r);
-            }
-        }
-        private int partition(int p, int r) {
-            int pivot = A[r];
-            int i = p-1;
-            int tmp;
-            for (int j=p; j<r; j++) {
-                if (A[j] <= pivot) {
-                    i++;
-                    tmp = A[i];
-                    A[i] = A[j];
-                    A[j] = tmp;
-                }
-            }
-            tmp = A[i+1];
-            A[i+1] = A[r];
-            A[r] = tmp;
-            return i+1;
-        }
-	}
-    
+
     public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	int n = Integer.parseInt(br.readLine());
@@ -53,8 +17,8 @@ public class Main {
     		return;
     	}
     	
-    	Sorting s = new Sorting(A);
-    	s.quickSort();
+        Arrays.sort(A);
+        
     	int a, b, count = 0;
     	for (int i=0; i<n; i++) {
     		a=0; b=n-1;
