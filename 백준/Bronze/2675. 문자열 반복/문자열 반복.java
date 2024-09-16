97d43fc888a37d90700ddd00318ce666b551e1f6
Jeon Hyeon Min <textneckdeveloper@gmail.com>
2024-09-16 20:00:01 +0900

    [Bronze II] Title: 문자열 반복, Time: 96 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int t = Integer.parseInt(br.readLine());
+		for (int i=1; i<=t; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int loop = Integer.parseInt(st.nextToken());
+			String s = st.nextToken();
+			for (int j=0; j<s.length(); j++) {
+				for (int count=1; count<=loop; count++) bw.write(s.charAt(j));
+			} bw.newLine();
+		}
+		bw.flush(); bw.close(); br.close();
+	}
+}
