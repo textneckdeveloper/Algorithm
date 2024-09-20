49256e1a0df734dff85d2bf7e4618be6b94d5017
Jeon Hyeon Min <textneckdeveloper@gmail.com>
2024-09-20 16:12:58 +0900

    [Silver IV] Title: 동전 0, Time: 372 ms, Memory: 16176 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	int n = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken());
+    	int[] money = new int[n]; int count = 0;
+    	for (int i=0; i<n; i++) money[i] = Integer.parseInt(br.readLine());
+    	for (int i=n-1; i>=0; i--) {
+    		while (k/money[i] != 0) {
+    			count++; k -= money[i];
+    		}
+    	} System.out.print(count);
+    }
+}
