e897ecedc684c69631e978a9fb412d9865058897
Jeon Hyeon Min <textneckdeveloper@gmail.com>
2024-09-30 22:12:46 +0900

    [Gold III] Title: 게임 개발, Time: 228 ms, Memory: 21684 KB -BaekjoonHub

---
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static List<Integer> graph[];
+	static int[] time;
+	static PrintWriter pw;
+	
+	static class Node {
+		int w, cost;
+		
+		Node(int w, int cost) {
+			this.w = w;
+			this.cost = cost;
+		}
+	}
+	
+	static void topologicalSort(int n) {
+		int[] inDegree = new int[n+1];
+		for (int i=1; i<=n; i++) for (int j : graph[i]) inDegree[j]++;
+		Queue<Integer> queue = new LinkedList<>();
+		for (int i=1; i<=n; i++) if (inDegree[i] == 0) queue.add(i);
+		int[] result = new int[n+1];
+		while (!queue.isEmpty()) {
+			int node = queue.poll();
+			for (int i : graph[node]) {
+				inDegree[i]--;
+				result[i] = Math.max(result[i], result[node] + time[node]);
+				if (inDegree[i] == 0) queue.add(i);
+			}
+		} for (int i=1; i<=n; i++) pw.println(result[i] + time[i]);
+	}
+
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	pw = new PrintWriter(System.out);
+    	int n = Integer.parseInt(br.readLine());
+    	graph = new ArrayList[n+1];
+    	for (int i=0; i<graph.length; i++) graph[i] = new ArrayList<>();
+		time = new int[n+1];
+    	for (int i=1; i<=n; i++) { StringTokenizer st = new StringTokenizer(br.readLine());
+    		time[i] = Integer.parseInt(st.nextToken());
+    		while (true) {
+    			int val = Integer.parseInt(st.nextToken());
+    			if (val == -1) break;
+    			graph[val].add(i);
+    		}
+    	} topologicalSort(n);
+    	pw.flush(); pw.close(); br.close();
+    }
+    
+}
